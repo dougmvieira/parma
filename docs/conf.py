afb8f533b55516b9e5fec8c6184826e635970b3c
Doug V <dougmvieira@gmail.com>
2019-09-18 01:34:11 +0100

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'parma'
 year = '2019'
 author = 'Douglas Machado Vieira'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.0'
+version = release = '0.1.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*names, **kwargs):
 
 setup(
     name='parma',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT license',
     description='Multivariate polyharmonic spline interpolation',
     long_description='%s\n%s' % (
